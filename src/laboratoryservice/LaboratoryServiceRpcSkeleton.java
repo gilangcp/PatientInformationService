29f1287163005b97b177c79ae5d467b3e83b8a33
gilang <gilang.cp@gmail.com>
2017-03-20 14:02:42 +0100

    Implement Patient & Diagnose Service

--- src/laboratoryservice/LaboratoryServiceRpcSkeleton.java
+++ src/laboratoryservice/LaboratoryServiceRpcSkeleton.java
@@ -6,11 +6,27 @@
  * by the Apache Axis2 version: 1.6.4  Built on : Dec 28, 2015 (10:03:39 GMT)
  */
     package laboratoryservice;
-    /**
+
+import java.util.Random;
+
+import prescriptiondiagnosetypes.Diagnose;
+import prescriptiondiagnosetypes.DiagnoseList;
+import prescriptiondiagnosetypes.Prescription;
+import prescriptiondiagnosetypes.PrescriptionList;
+
+/**
      *  LaboratoryServiceRpcSkeleton java skeleton for the axisService
      */
     public class LaboratoryServiceRpcSkeleton{
-        
+    	private Random rand = new Random();
+    	public String[] diagnoseName= new String[]{
+    			"X-Rays",
+    			"Blood Test",
+    			"Stool Exam",
+    			"Urine Test",
+    			"TBC Exam",
+    			"Cancer Test"    	
+    	};
          
         /**
          * Auto generated method signature
@@ -24,9 +40,21 @@
                   laboratoryservice.GetDiagnoseDetail getDiagnoseDetail
                   )
             {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getDiagnoseDetail");
-        }
+                     //Randomize the number of prescription
+                  	 int diagnoseCount = rand.nextInt((5 - 1) + 1) + 1;
+                  	 DiagnoseList diaglist = new DiagnoseList();
+                  	 
+                  	 for (int w=0; w < diagnoseCount; w++){
+                  		 Diagnose p = new Diagnose();
+                    	 int typeIndex = rand.nextInt(6);
+  	                	 p.setDescription(diagnoseName[typeIndex]);
+  	                	 p.setQuantity(1);
+  	                	 diaglist.addDiagnoseItem(p);
+  	                }
+                  	GetDiagnoseDetailResponse res = new GetDiagnoseDetailResponse();
+                  	res.setDiagnoseList(diaglist);
+                  	return res;
+             }
      
     }
     
--- src/patientservice/PatientServiceRpcSkeleton.java
+++ src/patientservice/PatientServiceRpcSkeleton.java
@@ -6,12 +6,41 @@
  * by the Apache Axis2 version: 1.6.4  Built on : Dec 28, 2015 (10:03:39 GMT)
  */
     package patientservice;
-    /**
+
+import java.util.Random;
+
+import invoiceservice.GetPrescriptionAndDiagnosisDetailResponse;
+import prescriptiondiagnosetypes.Prescription;
+import prescriptiondiagnosetypes.PrescriptionList;
+
+/**
      *  PatientServiceRpcSkeleton java skeleton for the axisService
      */
     public class PatientServiceRpcSkeleton{
         
-         
+    	private Random rand = new Random();
+    	public String[] medicineName= new String[]{
+    			"decitabine",
+    			"bromfenac",
+    			"dexpemedolac",
+    			"seractide",
+    			"tazadolene",
+    			"spiradolene",
+    			"levonantradol",
+    			"alprafenone",
+    			"dabuzalgron",
+    			"labetalol",
+    			"semaxanib"
+    	};
+    	
+    	public String[] ml = new String[]{
+    			"10",
+    			"20",
+    			"30",
+    			"40",
+    			"50"
+    	};
+    	
         /**
          * Auto generated method signature
          * 
@@ -22,11 +51,42 @@
                  public invoiceservice.GetPrescriptionAndDiagnosisDetailResponse getPrescriptionAndDiagnosisDetail
                   (
                   invoiceservice.GetPrescriptionAndDiagnosisDetail getPrescriptionAndDiagnosisDetail
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getPrescriptionAndDiagnosisDetail");
-        }
-     
+                  ){
+                     //Randomize the number of prescription
+                	 int prescriptionCount = rand.nextInt((5 - 1) + 1) + 1;
+                	 PrescriptionList presList = new PrescriptionList();
+                	 for (int w=0; w < prescriptionCount; w++){
+                		 Prescription p = new Prescription();
+                		 
+	                	 //Randomize the quantity of the medicine
+	                	 int medicineCount = rand.nextInt((10 - 1) + 1) + 1;
+	                	 
+	                	 String description ="";
+	                	 
+	                	 for(int x=0 ; x < medicineCount; x++){
+	                		 //Randomize the type of the medicine
+	                    	 int typeIndex = rand.nextInt(11);
+	                    	 int mlIndex = rand.nextInt(4);
+	                    	 description+= medicineName[typeIndex]+"-"+ml[mlIndex]+"ml\n";
+	                	 }
+	
+	                	 description+="--\n";
+	                	 
+	                	 //Randomize the daily intake
+	                	 int dailyIntake = rand.nextInt((3 - 1) + 1) + 1;
+	                	 description+=dailyIntake + "x daily\n";
+	                	 
+	                	 //Randomize the quantity
+	                	 int quantity = rand.nextInt((7 - 1) + 1) + 1;
+	                	 p.setDescription(description);
+	                	 p.setQuantity(quantity);
+	                	 presList.addPrescriptionItem(p);  
+	                }
+                	 
+                	 GetPrescriptionAndDiagnosisDetailResponse res = new GetPrescriptionAndDiagnosisDetailResponse();
+                	 res.setPrescription(presList);
+                	 return res;
+                 }   		 
+                		
     }
     
